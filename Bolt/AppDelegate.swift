9c635b7d88dc2dda4ffeaa7a12a083eb412eeda4
Robert Walker <robertwalker1@icloud.com>
2017-07-07 18:53:14 -0400

    Improve naming convention and structure
    Improves the naming of variables and functions.
    
    Improves handling of application state enums by use of swtich statements

--- Bolt/AppDelegate.swift
+++ Bolt/AppDelegate.swift
@@ -11,10 +11,12 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     let statusItem = NSStatusBar.system().statusItem(withLength: -2.0)
-    let sleepManager = SleepManager()
+    let sleepController = SleepController()
 
     @IBOutlet weak var siMenu: NSMenu!
     @IBOutlet weak var preventSleepMenuItem: NSMenuItem!
+    
+    // MARK: - Applicaiton life cycle
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         if let siButton = statusItem.button {
@@ -24,10 +26,32 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
         }
         siMenu.delegate = self
         statusItem.menu = siMenu
+        
+        print(statusItem.behavior)
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+
+    // MARK: - NSMenuDelegate conformance
+
+    func menuWillOpen(_ menu: NSMenu) {
+        updateIcon(imageName: "StatusItem")
+    }
+
+    func menuDidClose(_ menu: NSMenu) {
+        var imageName = "StatusItem"
+        
+        // The menu closes before the target's action is fired,
+        // so if we're about to switch away from .allowSleep then we want the alternate icon image
+        switch sleepController.sleepState {
+        case .allowSleep:
+            imageName = "StatusItemAlternate"
+        case .preventSleepIndefinitely:
+            imageName = "StatusItem"
+        }
+        updateIcon(imageName: imageName)
     }
 
     private func updateIcon(imageName: String) {
@@ -37,21 +61,28 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
             }
         }
     }
-
-    func menuWillOpen(_ menu: NSMenu) {
-        updateIcon(imageName: "StatusItem")
-    }
-
-    func menuDidClose(_ menu: NSMenu) {
-        let imageName = (sleepManager.sleepState == .preventSleep) ? "StatusItem" : "StatusItemAlternate"
-        updateIcon(imageName: imageName)
-    }
+    
+    // MARK: - StatusItem action
 
     @IBAction func preventSleepIndefinitely(_ sender: NSMenuItem) {
-        sleepManager.togglePreventSleep()
-        let title = sleepManager.sleepState == .preventSleep
-            ? NSLocalizedString("Allow System Sleep", comment: "Allow System to Sleep")
-            : NSLocalizedString("Prevent System Sleep", comment: "Prevent System from Sleeping")
-        preventSleepMenuItem.title = title
+        let preventSystemSleepTitle =
+            NSLocalizedString("Prevent System Sleep",
+                              comment: "Menu item title used to prevent system from activating sleep")
+        let allowSystemSleepTitle =
+            NSLocalizedString("Allow System Sleep",
+                              comment: "Menu item title used to allow system to activate sleep")
+        var desiredState = SleepState.allowSleep
+        
+        // Toggle between .allowSleep and .preventSleepIndefinitely
+        switch sleepController.sleepState {
+        case .allowSleep:
+            desiredState = .preventSleepIndefinitely
+            preventSleepMenuItem.title = allowSystemSleepTitle
+        case .preventSleepIndefinitely:
+            desiredState = .allowSleep
+            preventSleepMenuItem.title = preventSystemSleepTitle
+        }
+        
+        sleepController.updateSleepState(state: desiredState)
     }
 }
