b65af6622d25b72299bac9a0acdb3d2d773e0b80
Robert Walker <robertwalker1@icloud.com>
2017-07-06 21:26:12 -0400

    Add menu to status item
    Use a menu item to activate sleep prevention instead of the click action.

--- Bolt/AppDelegate.swift
+++ Bolt/AppDelegate.swift
@@ -9,31 +9,49 @@
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     let statusItem = NSStatusBar.system().statusItem(withLength: -2.0)
     let sleepManager = SleepManager()
 
+    @IBOutlet weak var siMenu: NSMenu!
+    @IBOutlet weak var preventSleepMenuItem: NSMenuItem!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         if let siButton = statusItem.button {
             if let image = NSImage(named: "StatusItem") {
                 siButton.image = image
-                siButton.target = self
-                siButton.action = #selector(AppDelegate.handleClick)
             }
         }
+        siMenu.delegate = self
+        statusItem.menu = siMenu
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-    
-    func handleClick() {
-        let imageName = (sleepManager.sleepState == .preventSleep) ? "StatusItem" : "StatusItemAlternate"
+
+    private func updateIcon(imageName: String) {
         if let siButton = statusItem.button {
             if let image = NSImage(named: imageName) {
                 siButton.image = image
-                sleepManager.togglePreventSleep()
             }
         }
+    }
+
+    func menuWillOpen(_ menu: NSMenu) {
+        updateIcon(imageName: "StatusItem")
+    }
+
+    func menuDidClose(_ menu: NSMenu) {
+        let imageName = (sleepManager.sleepState == .preventSleep) ? "StatusItem" : "StatusItemAlternate"
+        updateIcon(imageName: imageName)
+    }
+
+    @IBAction func preventSleepIndefinitely(_ sender: NSMenuItem) {
+        sleepManager.togglePreventSleep()
+        let title = sleepManager.sleepState == .preventSleep
+            ? NSLocalizedString("Allow System Sleep", comment: "Allow System to Sleep")
+            : NSLocalizedString("Prevent System Sleep", comment: "Prevent System from Sleeping")
+        preventSleepMenuItem.title = title
     }
 }
