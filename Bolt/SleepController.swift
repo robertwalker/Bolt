a4dbd6f915c7ef6548f1530ad0d34fa7a51a0641
Robert Walker <robertwalker1@icloud.com>
2017-07-07 21:59:51 -0400

    Fix state change bug

--- Bolt/SleepController.swift
+++ Bolt/SleepController.swift
@@ -27,16 +27,21 @@ class SleepController {
     func updateSleepState(state newState: SleepState) {
         let reasonForActivity = "Bolt Controls System Sleep" as CFString
         
-        switch newState {
+        switch sleepState {
         case .allowSleep:
+            // Prevent display sleep
             displaySleep.success =
                 IOPMAssertionCreateWithName(kIOPMAssertionTypePreventUserIdleDisplaySleep as CFString!,
                                             IOPMAssertionLevel(kIOPMAssertionLevelOn),
                                             reasonForActivity, &displaySleep.assertionID)
+            
+            // Prevent system sleep
             systemSleep.success =
                 IOPMAssertionCreateWithName(kIOPMAssertionTypePreventUserIdleSystemSleep as CFString!,
                                             IOPMAssertionLevel(kIOPMAssertionLevelOn),
                                             reasonForActivity, &systemSleep.assertionID)
+            
+            // Update current state
             sleepState = newState
         case .preventSleepIndefinitely:
             if displaySleep.success == kIOReturnSuccess {
